507c06d4b7c8ebbfec4501954281d3b5352d61c0
aehamdy <ae.hamdy91@gmail.com>
2024-01-24 22:07:00 +0200

    Added comments to the challenge-2.js file

--- scripts/challenge-2.js
+++ scripts/challenge-2.js
@@ -50,28 +50,31 @@ const skillsArray = [
 // Calculate years of experience automatically check ✅
 const currentYear = new Date().getFullYear() // get current year
 
+
+// Create dynamically HTML structure based on skillsArray data
 const createSkillsList = (listItem) => `<li class="skills__column" data-name="${listItem.name}" data-type="${listItem.type}">
 <h2 class="skills__title">${listItem.name}</h2>
 <span class="skills__years">${currentYear - listItem.experience} Years</span>
 </li>`;
 
-const skillsList = skillsArray.map((skill) => createSkillsList(skill)).join("");
+const skillsList = skillsArray.map((skill) => createSkillsList(skill)).join(""); // Convert to string
 
 // console.log(skillsList);
 
-const ul = `<ul class="skills__list">${skillsList}</ul>`
+const ul = `<ul class="skills__list">${skillsList}</ul>`; // Create an unordered list and add all skills inside it
 
 const skillsSection = document.querySelector("#skills");
 
-skillsSection.insertAdjacentHTML("beforeend", ul);
+skillsSection.insertAdjacentHTML("beforeend", ul); // Add the ul in the section
 
 
 /* ✨ Bonus ✨ */
 // Add filters to skills section ✅
+
 document.querySelectorAll(".skills__button").forEach((element) => { 
-  element.addEventListener("click", () => { 
-    document.querySelector(".skills__button--isActive").classList.remove("skills__button--isActive");
-    element.classList.add("skills__button--isActive");
-    document.querySelector(".skills__list").classList = `skills__list ${element.dataset.type}`;
+  element.addEventListener("click", () => {    // add click event on each button
+    document.querySelector(".skills__button--isActive").classList.remove("skills__button--isActive"); // remove the class from each button
+    element.classList.add("skills__button--isActive"); // add class on the button which is clicked
+    document.querySelector(".skills__list").classList = `skills__list ${element.dataset.type}`; // add a new class to the ul depending on the clicked button
   })
-})
+});
